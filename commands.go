cad53812fb112e557731aab7b65f3b0353bb2a8b
Bob Lail <lail@squareup.com>
2025-12-10 09:26:53 -0800

    perf: Parallelize expanding commands

--- commands.go
+++ commands.go
@@ -1,5 +1,7 @@
 package exoskeleton
 
+import "sync"
+
 type Commands []Command
 
 // Find returns the first command with a given name.
@@ -49,25 +51,54 @@ func WithoutExpandedModules() ExpandOption {
 }
 
 func expand(c Commands, depth int, includeExpandedModules bool) (Commands, []error) {
+	type result struct {
+		commands Commands
+		errors   []error
+	}
+	results := make([]result, len(c))
+	var wg sync.WaitGroup
+
+	for i, cmd := range c {
+		// If this is a module, recursively flatten its subcommands...
+		if m, ok := cmd.(Module); ok && depth != 0 {
+			// ...and do that in parallel because resolving subcommands may involve
+			// executing them with `--describe-commands` and subcommands that accidentally
+			// introduce latency impact the experience.
+			wg.Add(1)
+			go func(idx int, mod Module) {
+				defer wg.Done()
+				cmds := Commands{}
+				errs := []error{}
+
+				if includeExpandedModules {
+					cmds = append(cmds, mod)
+				}
+
+				subcmds, err := mod.Subcommands()
+				if err != nil {
+					errs = append(errs, err)
+				}
+
+				fcmds, ferrs := expand(subcmds, depth-1, includeExpandedModules)
+				cmds = append(cmds, fcmds...)
+				errs = append(errs, ferrs...)
+
+				// Store result at the correct index to maintain order
+				results[idx] = result{commands: cmds, errors: errs}
+			}(i, m)
+
+		} else {
+			results[i] = result{commands: Commands{cmd}, errors: []error{}}
+		}
+	}
+
+	wg.Wait()
+
 	all := Commands{}
 	errs := []error{}
-
-	for _, cmd := range c {
-		if m, ok := cmd.(Module); ok && depth != 0 {
-			if includeExpandedModules {
-				all = append(all, cmd)
-			}
-
-			subcmds, err := m.Subcommands()
-			if err != nil {
-				errs = append(errs, err)
-			}
-			fcmds, ferrs := expand(subcmds, depth-1, includeExpandedModules)
-			all = append(all, fcmds...)
-			errs = append(errs, ferrs...)
-		} else {
-			all = append(all, cmd)
-		}
+	for _, r := range results {
+		all = append(all, r.commands...)
+		errs = append(errs, r.errors...)
 	}
 
 	return all, errs
